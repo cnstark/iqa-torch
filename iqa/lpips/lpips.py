485d1d6012c756611a5a6b9d4322b1734e82f1c2
Yuhao Wang <45590791+cnstark@users.noreply.github.com>
2022-02-13 22:52:45 +0800

    feature: LPIPS (#4)
    * feature: LPIPS
    
    * fix

---
+++ iqa/lpips/lpips.py
@@ -0,0 +1,132 @@
+from typing import Tuple
+import torch
+from torch import nn
+
+from .lpips_model import LPIPSModel
+from ..utils import crop_border, convert_image_dtype, reorder_image
+
+
+__all__ = ['lpips', 'LPIPS']
+
+
+def _preprocess_imgs(
+    img1: torch.Tensor,
+    img2: torch.Tensor,
+    crop_size: int = 0,
+    input_order: str = 'NCHW',
+) -> Tuple[torch.Tensor]:
+    """Preprocessing images for calculate LPIPS.
+
+    Args:
+        img1 (torch.Tensor): Image 1.
+        img2 (torch.Tensor): Image 2.
+        crop_size (int, optional): Crop border for each end of height and weight. Defaults to 0.
+        input_order (str, optional): The order of input image. Defaults to 'NCHW'.
+
+    Returns:
+        Tuple[torch.Tensor]: Preprocessed images.
+    """
+    if img1.size() != img2.size():
+        raise ValueError(f'Image shapes are different: {img1.shape}, {img2.shape}.')
+
+    if crop_size != 0:
+        img1 = crop_border(img1, crop_size, input_order)
+        img2 = crop_border(img2, crop_size, input_order)
+
+    img1 = convert_image_dtype(img1, torch.float32)
+    img2 = convert_image_dtype(img2, torch.float32)
+
+    img1 = reorder_image(img1, input_order, 'NCHW')
+    img2 = reorder_image(img2, input_order, 'NCHW')
+
+    return img1, img2
+
+
+def lpips(
+    img1: torch.Tensor,
+    img2: torch.Tensor,
+    net: str = 'alex',
+    version: str = '0.1',
+    crop_size: int = 0,
+    input_order: str = 'NCHW',
+) -> torch.Tensor:
+    """Calculate LPIPS (Learned Perceptual Image Patch Similarity).
+
+    Ref: https://github.com/richzhang/PerceptualSimilarity
+
+    Note:
+        Since `nn.Module` are included in LPIPS, it is recommended to use `class LPIPS`.
+
+    Note:
+        The dtype and the range of images should be:
+
+            1. torch.uint8 type with range [0, 255];
+            2. torch.float32 type with range [0, 1].
+
+    Args:
+        img1 (torch.Tensor): Image 1.
+        img2 (torch.Tensor): Image 2.
+        net (str, optional): ['alex','vgg','squeeze'] are the base/trunk networks
+            available. Defaults to 'alex'.
+        version (str, optional): LPIPS version, corresponds to old arxiv v1
+            (https://arxiv.org/abs/1801.03924v1). Defaults to '0.1'.
+        crop_size (int, optional): Crop border for each end of height and weight. Defaults to 0.
+        input_order (str, optional): The order of input image. Defaults to 'NCHW'.
+
+    Returns:
+        torch.Tensor: LPIPS result.
+    """
+    img1, img2 = _preprocess_imgs(img1, img2, crop_size, input_order)
+
+    lpips_model = LPIPSModel(net=net, version=version)
+    lpips_model.to(img1.device)
+
+    return lpips_model(img1, img2)
+
+
+class LPIPS(nn.Module):
+    def __init__(
+        self,
+        net: str = 'alex',
+        version: str = '0.1',
+        crop_size: int = 0,
+        input_order: str = 'NCHW',
+    ):
+        """LPIPS (Learned Perceptual Image Patch Similarity) calculator.
+
+        Ref: https://github.com/richzhang/PerceptualSimilarity
+
+        Args:
+            net (str, optional): ['alex','vgg','squeeze'] are the base/trunk networks
+                available. Defaults to 'alex'.
+            version (str, optional): LPIPS version, corresponds to old arxiv v1
+                (https://arxiv.org/abs/1801.03924v1). Defaults to '0.1'.
+            crop_size (int, optional): Crop border for each end of height and weight. Defaults to 0.
+            input_order (str, optional): The order of input image. Defaults to 'NCHW'.
+        """
+        super().__init__()
+
+        self.crop_size = crop_size
+        self.input_order = input_order
+
+        self.lpips_model = LPIPSModel(net=net, version=version)
+
+    def forward(self, img1: torch.Tensor, img2: torch.Tensor) -> torch.Tensor:
+        """Calculate LPIPS (Learned Perceptual Image Patch Similarity).
+
+        Note:
+            The dtype and the range of images should be:
+
+                1. torch.uint8 type with range [0, 255];
+                2. torch.float32 type with range [0, 1].
+
+        Args:
+            img1 (torch.Tensor): Image 1.
+            img2 (torch.Tensor): Image 2.
+
+        Returns:
+            torch.Tensor: LPIPS result.
+        """
+        img1, img2 = _preprocess_imgs(img1, img2, self.crop_size, self.input_order)
+
+        return self.lpips_model(img1, img2)
---
+++ iqa/lpips/lpips_model.py
@@ -0,0 +1,195 @@
+# https://github.com/richzhang/PerceptualSimilarity/blob/master/lpips/lpips.py
+# https://github.com/richzhang/PerceptualSimilarity/blob/master/lpips/__init__.py
+import os
+import inspect
+
+import torch
+from torch import nn
+
+from . import pretrained_networks as pn
+
+
+def normalize_tensor(in_feat, eps=1e-10):
+    norm_factor = torch.sqrt(torch.sum(in_feat ** 2, dim=1, keepdim=True))
+    return in_feat / (norm_factor + eps)
+
+
+def spatial_average(in_tens, keepdim=True):
+    return in_tens.mean([2, 3], keepdim=keepdim)
+
+
+def upsample(in_tens, out_HW=(64, 64)):
+    return nn.Upsample(size=out_HW, mode='bilinear', align_corners=False)(in_tens)
+
+
+class ScalingLayer(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.register_buffer(
+            'shift', torch.Tensor([-0.030, -0.088, -0.188])[None, :, None, None]
+        )
+        self.register_buffer(
+            'scale', torch.Tensor([0.458, 0.448, 0.450])[None, :, None, None]
+        )
+
+    def forward(self, inp):
+        return (inp - self.shift) / self.scale
+
+
+class NetLinLayer(nn.Module):
+    def __init__(self, chn_in, chn_out=1, use_dropout=False):
+        super().__init__()
+
+        layers = []
+        if use_dropout:
+            layers.append(nn.Dropout())
+        layers.append(nn.Conv2d(chn_in, chn_out, 1, stride=1, padding=0, bias=False))
+
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class LPIPSModel(nn.Module):
+    def __init__(
+        self,
+        pretrained=True,
+        net='alex',
+        version='0.1',
+        lpips=True,
+        spatial=False,
+        pnet_rand=False,
+        pnet_tune=False,
+        use_dropout=True,
+        model_path=None,
+        eval_mode=True,
+    ):
+        """Initializes a perceptual loss torch.nn.Module
+        Parameters (default listed first)
+        ---------------------------------
+        lpips : bool
+            [True] use linear layers on top of base/trunk network
+            [False] means no linear layers; each layer is averaged together
+        pretrained : bool
+            This flag controls the linear layers, which are only in effect when lpips=True above
+            [True] means linear layers are calibrated with human perceptual judgments
+            [False] means linear layers are randomly initialized
+        pnet_rand : bool
+            [False] means trunk loaded with ImageNet classification weights
+            [True] means randomly initialized trunk
+        net : str
+            ['alex','vgg','squeeze'] are the base/trunk networks available
+        version : str
+            ['v0.1'] is the default and latest
+            ['v0.0'] contained a normalization bug; corresponds to old arxiv v1 (https://arxiv.org/abs/1801.03924v1)
+        model_path : 'str'
+            [None] is default and loads the pretrained weights from paper https://arxiv.org/abs/1801.03924v1
+        The following parameters should only be changed if training the network
+        eval_mode : bool
+            [True] is for test mode (default)
+            [False] is for training mode
+        pnet_tune
+            [False] keep base/trunk frozen
+            [True] tune the base/trunk network
+        use_dropout : bool
+            [True] to use dropout when training linear layers
+            [False] for no dropout when training linear layers
+        """
+        super().__init__()
+
+        self.spatial = spatial
+        self.lpips = lpips  # false means baseline of just averaging all layers
+        self.version = version
+        self.scaling_layer = ScalingLayer()
+
+        if net in ['vgg', 'vgg16']:
+            net_type = pn.vgg16
+            self.chns = [64, 128, 256, 512, 512]
+        elif net == 'alex':
+            net_type = pn.alexnet
+            self.chns = [64, 192, 384, 256, 256]
+        elif net == 'squeeze':
+            net_type = pn.squeezenet
+            self.chns = [64, 128, 256, 384, 384, 512, 512]
+        self.L = len(self.chns)
+
+        self.net = net_type(pretrained=not pnet_rand, requires_grad=pnet_tune)
+
+        if lpips:
+            self.lin0 = NetLinLayer(self.chns[0], use_dropout=use_dropout)
+            self.lin1 = NetLinLayer(self.chns[1], use_dropout=use_dropout)
+            self.lin2 = NetLinLayer(self.chns[2], use_dropout=use_dropout)
+            self.lin3 = NetLinLayer(self.chns[3], use_dropout=use_dropout)
+            self.lin4 = NetLinLayer(self.chns[4], use_dropout=use_dropout)
+            self.lins = [self.lin0, self.lin1, self.lin2, self.lin3, self.lin4]
+            if net == 'squeeze':  # 7 layers for squeezenet
+                self.lin5 = NetLinLayer(self.chns[5], use_dropout=use_dropout)
+                self.lin6 = NetLinLayer(self.chns[6], use_dropout=use_dropout)
+                self.lins += [self.lin5, self.lin6]
+            self.lins = nn.ModuleList(self.lins)
+
+            if pretrained:
+                if model_path is None:
+                    model_path = os.path.abspath(
+                        os.path.join(
+                            inspect.getfile(self.__init__),
+                            '..',
+                            'weights/v%s/%s.pth' % (version, net),
+                        )
+                    )
+
+                self.load_state_dict(
+                    torch.load(model_path, map_location='cpu'), strict=False
+                )
+
+        if eval_mode:
+            self.eval()
+
+    def forward(self, in0, in1, retPerLayer=False):
+        # v0.0 - original release had a bug, where input was not scaled
+        in0_input, in1_input = (
+            (self.scaling_layer(in0), self.scaling_layer(in1))
+            if self.version == '0.1'
+            else (in0, in1)
+        )
+        outs0, outs1 = self.net.forward(in0_input), self.net.forward(in1_input)
+        feats0, feats1, diffs = {}, {}, {}
+
+        for kk in range(self.L):
+            feats0[kk], feats1[kk] = normalize_tensor(outs0[kk]), normalize_tensor(
+                outs1[kk]
+            )
+            diffs[kk] = (feats0[kk] - feats1[kk]) ** 2
+
+        if self.lpips:
+            if self.spatial:
+                res = [
+                    upsample(self.lins[kk](diffs[kk]), out_HW=in0.shape[2:])
+                    for kk in range(self.L)
+                ]
+            else:
+                res = [
+                    spatial_average(self.lins[kk](diffs[kk]), keepdim=True)
+                    for kk in range(self.L)
+                ]
+        else:
+            if self.spatial:
+                res = [
+                    upsample(diffs[kk].sum(dim=1, keepdim=True), out_HW=in0.shape[2:])
+                    for kk in range(self.L)
+                ]
+            else:
+                res = [
+                    spatial_average(diffs[kk].sum(dim=1, keepdim=True), keepdim=True)
+                    for kk in range(self.L)
+                ]
+
+        val = 0
+        for l in range(self.L):
+            val += res[l]
+
+        if retPerLayer:
+            return (val, res)
+        else:
+            return val
