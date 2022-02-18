1d990505ee0e72aad76cf1af2a046cbf25610554
Yuhao Wang <yuhaow97@gmail.com>
2022-02-18 10:35:16 +0800

    feature: csc rgb2gray

--- tests/test_csc.py
+++ tests/test_csc.py
@@ -4,10 +4,11 @@ import unittest
 import functools
 sys.path.append(os.getcwd())
 
+import cv2
 import numpy as np
 import torch
 
-from iqa.csc import rgb2ycbcr, rgb2y, ycbcr2rgb
+from iqa.csc import rgb2gray, rgb2ycbcr, rgb2y, ycbcr2rgb
 from libs.basicsr_matlab_functions import rgb2ycbcr as rgb2ycbcr_bsr
 from libs.basicsr_matlab_functions import ycbcr2rgb as ycbcr2rgb_bsr
 
@@ -34,6 +35,22 @@ class CSCTestCase(unittest.TestCase):
                 # TODO: ycbcr2rgb precision
                 self.assertLess(diff.max(), 1e-5)
                 self.assertLess(diff.mean(), 1e-5)
+
+    def test_rgb2gray(self):
+        for shape in IMAGE_SHAPES:
+            if len(shape) == 3:
+                with self.subTest(shape=shape):
+                    img_in = np.random.rand(*shape).astype(np.float32)
+                    img_in_torch = torch.from_numpy(img_in)
+
+                    img_out = cv2.cvtColor(img_in, cv2.COLOR_RGB2GRAY)
+                    img_out_torch = rgb2gray(img_in_torch)
+
+                    if img_out.ndim != len(shape):
+                        img_out = img_out[..., None]
+                    diff = np.abs(img_out - img_out_torch.numpy())
+                    self.assertLess(diff.max(), 1e-5)
+                    self.assertLess(diff.mean(), 1e-5)
 
     def test_rgb2ycbcr(self):
         self._test_csc(rgb2ycbcr, rgb2ycbcr_bsr)
