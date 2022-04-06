9b48c172a2a6803f96e2ed1db8432ce0e70a8e34
Yuhao Wang <yuhaow97@gmail.com>
2022-04-06 21:59:41 +0800

    feature: pypi deploy

--- iqa/__init__.py
+++ iqa/__init__.py
@@ -2,3 +2,12 @@ from .psnr import *
 from .ssim import *
 from .lpips import *
 from .niqe import *
+
+from .version import __version__
+
+
+__all__ = [
+    'psnr', 'PSNR', 'ssim', 'SSIM',
+    'lpips', 'LPIPS', 'niqe', 'NIQE',
+    '__version__'
+]
